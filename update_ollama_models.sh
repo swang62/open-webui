10b119dfe21b452833a30474343e90aab4696a01
Steve Wang <stevew77@gmail.com>
2024-06-06 00:53:11 +0800

    chore: update llm script

--- update_ollama_models.sh
+++ update_ollama_models.sh
@@ -2,9 +2,9 @@
 # update_llm.sh
 
 # Retrieves the list of LLMs installed in the Docker container
-llm_list=$(docker exec ollama ollama list | tail -n +2 | awk '{print $1}')
+llm_list=$(ollama list | tail -n +2 | awk '{print $1}')
 
 # Loop over each LLM to update it
 for llm in $llm_list; do
-  docker exec ollama ollama pull $llm
+  ollama pull $llm
 done
